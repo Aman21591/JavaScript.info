4c204ff47db10e43c23e22cd6340a8db2aac1b22
Aman_Yadav <19BCS2461@gmail.com>
2022-01-07 22:32:08 +0530

    javaScript Tutorial

---
+++ js/dataType.js
@@ -0,0 +1,30 @@
+let str="Hello";
+let str2="I am Aman Yadav";
+let phrase=`${str2}`;
+console.log(phrase);
+
+let name="jhon";
+alert(`Hello ${name}! `);
+alert(`the result is ${1+4}`);
+
+// Boolean
+let isGreater=4<1;
+alert(isGreater);
+
+let age=14;
+alert(age);
+
+// type of operator
+
+typeof(0);
+console.log(typeof(0));
+console.log(typeof(null));
+console.log(typeof(Math));
+console.log(typeof(10n));
+console.log(typeof(alert));
+
+//Example
+// let name="Aman";
+// alert(`hello ${1}`);
+// alert(`hello ${"name"}`);
+// alert(`hello ${name}`);
---
+++ js/variable.js
@@ -0,0 +1,33 @@
+// let Name;
+// Name ="Aman Yadav";
+// let Given;
+// Given=Name;
+// alert(Name);
+// alert(Given);
+
+// "use strict";
+// const COLOR_RED="#F00";
+// const COLOR_GREEN = "#0F0";
+// const COLOR_BLUE = "#00F";
+// const COLOR_ORANGE = "#FF7F00";
+
+
+// let color=COLOR_RED;
+// alert(color);
+
+
+// let admin;
+// let name;
+// name="Jhon";
+// admin=name;
+// alert(name);
+// alert(admin);
+// let ourPlanet="Earth";
+// let currentUserName="jhon";
+// alert(1/0);
+// alert(Infinity);
+// alert("not a number"/2);
+
+
+
+
