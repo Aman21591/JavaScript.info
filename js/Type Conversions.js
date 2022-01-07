ec24e30c767c2251b73503aa4d9034a71a29a15b
Aman_Yadav <19BCS2461@gmail.com>
2022-01-07 23:16:48 +0530

    Type Conversion

---
+++ js/Type Conversions.js
@@ -0,0 +1,22 @@
+// console.log('Hello');
+// //String Conversion
+
+// let value=true;
+// alert(typeof value);
+
+// value=String(value);
+// alert(typeof value);
+
+// //Numeric Conversion
+// let number;
+// alert("6"/"3");
+
+//Numeric Conversion
+alert(Number("123"));
+alert(Number(true));
+alert(Number(false));
+
+//Boolean Conversion
+alert(Boolean(1));
+alert(Boolean(0));
+alert(Boolean(true));
