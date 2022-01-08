26e6cbd5e74faba1a9730b74d8d355b686a02bb6
Aman_Yadav <19BCS2461@gmail.com>
2022-01-08 10:04:48 +0530

    Basic Operators

--- js/Type Conversions.js
+++ js/Type Conversions.js
@@ -1,15 +1,15 @@
-// console.log('Hello');
-// //String Conversion
+console.log('Hello');
+//String Conversion
 
-// let value=true;
-// alert(typeof value);
+let value=true;
+alert(typeof value);
 
-// value=String(value);
-// alert(typeof value);
+value=String(value);
+alert(typeof value);
 
-// //Numeric Conversion
-// let number;
-// alert("6"/"3");
+//Numeric Conversion
+let number;
+alert("6"/"3");
 
 //Numeric Conversion
 alert(Number("123"));
---
+++ js/basicOperator.js
@@ -0,0 +1,61 @@
+console.log("Basic Operator");
+// //Unary , binary ,operand
+// let x=1;
+// x=-x;
+// alert(x);
+
+// let a=5,b=3;
+// alert(a-b);
+// console.log(a-b);
+
+// alert(10+2);
+// alert(10-2);
+// alert(10*2);
+// alert(10/2);
+// alert(10**2);
+// alert(10%2);
+// alert(10**(1/2));
+
+// //string concatenation
+
+// let s="my" + "string";
+// alert(s);
+
+console.log('1'+2);
+console.log('2'+1);
+console.log('2'+1+1);
+console.log(1+1+'2');
+
+let a=1,b=2;
+let c=5-(a=b+1);
+console.log(a);
+console.log(c);
+
+//postfix and prefix
+
+let d=5;
+d++;
+console.log(d);
+
+let counter=6;
+counter++;
+let e=counter++;
+console.log(e);
+
+let prefix=1;
+console.log(2* ++ prefix);
+
+let add=(5+2,1+3)
+console.log(add);
+
+let f=1,g=1;
+console.log(++f);
+console.log(g++);
+console.log(f);
+console.log(g);
+
+
+
+
+
+
